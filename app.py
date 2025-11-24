7c28a5b0f750e810fba5747aa3b972780135881e
kaksart60-sketch <kaksart60@gmail.com>
2025-11-24 19:36:26 +0530

    Fix render host and port

--- app.py
+++ app.py
@@ -46,6 +46,9 @@ def memory_lock(mid):
     return render_template("memory_lock.html", memory=memory, mid=mid)
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
